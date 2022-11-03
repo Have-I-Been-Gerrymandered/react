7dfdb7a13136e54a50f1528df3ac325757a68f4f
swest20 <36340998+swest20@users.noreply.github.com>
2022-11-03 16:09:14 -0400

    Add files via upload
    Once the state is selected map for that state pops up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,111 @@
 import React, { Component } from 'react';
 import USAMap from "react-usa-map";
+import Select from 'react-select'
 //import Dropdown from 'react-bootstrap/Dropdown';
 import './App.css';
 
 class App extends Component {
-    /* mandatory */
-    mapHandler = (event) => {
-        alert(event.target.dataset.name);
-    };
 
-    render() {
-        return (
-            <div class="App">
-                <USAMap onClick={this.mapHandler} />
-            </div>
-        );
+  constructor(props){
+    super(props)
+    this.state = {
+      selectOptions : [],
+      id: '',
+      name: '',
     }
+  }
+
+ async getOptions(){
+    const states = [
+      {value: "AL", label: "Alabama"},
+      {value: "AK", label: "Alaska"},
+      {value: "AZ", label :  "Arizona"},
+      {value: "AR", label :  "Arkansas"},
+      {value: "CA", label :  "California"},
+      {value: "CO", label :  "Colorado"},
+      {value: "CT", label :  "Connecticut"},
+      {value: "DE", label :  "Delaware"},
+      {value: "DC", label :  "District Of Columbia"},
+      {value: "FL", label :  "Florida"},
+      {value: "GA", label :  "Georgia"},
+      {value: "HI", label :  "Hawaii"},
+      {value: "ID", label :  "Idaho"},
+      {value: "IL", label :  "Illinois"},
+      {value: "IN", label :  "Indiana"},
+      {value: "IA", label :  "Iowa"},
+      {value: "KS", label :  "Kansas"},
+      {value: "KY", label :  "Kentucky"},
+      {value: "LA", label :  "Louisiana"},
+      {value: "ME", label :  "Maine"},
+      {value: "MD", label :  "Maryland"},
+      {value: "MA", label :  "Massachusetts"},
+      {value: "MI", label :  "Michigan"},
+      {value: "MN", label :  "Minnesota"},
+      {value: "MS", label :  "Mississippi"},
+      {value: "MO", label :  "Missouri"},
+      {value: "MT", label :  "Montana"},
+      {value: "NE", label :  "Nebraska"},
+      {value: "NV", label :  "Nevada"},
+      {value: "NH", label :  "New Hampshire"},
+      {value: "NJ", label :  "New Jersey"},
+      {value: "NM", label :  "New Mexico"},
+      {value: "NY", label :  "New York"},
+      {value: "NC", label :  "North Carolina"},
+      {value: "ND", label :  "North Dakota"},
+      {value: "OH", label :  "Ohio"},
+      {value: "OK", label :  "Oklahoma"},
+      {value: "OR", label :  "Oregon"},
+      {value: "PA", label :  "Pennsylvania"},
+      {value: "RI", label :  "Rhode Island"},
+      {value: "SC", label :  "South Carolina"},
+      {value: "SD", label :  "South Dakota"},
+      {value: "TN", label :  "Tennessee"},
+      {value: "TX", label :  "Texas"},
+      {value: "UT", label :  "Utah"},
+      {value: "VT", label :  "Vermont"},
+      {value: "VA", label :  "Virginia"},
+      {value: "WA", label :  "Washington"},
+      {value: "WV", label :  "West Virginia"},
+      {value: "WI", label :  "Wisconsin"},
+      {value: "WY", label :  "Wyoming"}, ];
+
+    this.setState({selectOptions: states})
+
+  }
+
+  handleChange(e){
+   this.setState({id:e.value, name:e.label});
+  }
+
+  componentDidMount(){
+      this.getOptions()
+  }
+
+
+  mapHandler = (event) => {
+    alert(event.target.dataset.name);
+};
+
+  render() {
+    console.log(this.state.selectOptions)
+    return (
+      <div class="App">
+        <h1 class="title">Have I been Gerrymandered?</h1>
+        <b class={"selector-child"}> Choose Your State! </b>
+        <Select options={this.state.selectOptions} onChange={this.handleChange.bind(this)} />
+        <div class={"selector-class"}>
+        <a href="https://www.house.gov/representatives/find-your-representative">
+            <button> Find my District</button>
+        </a>
+      </div>
+        <p>You have selected <strong>{this.state.name}</strong> whose id is <strong>{this.state.id}</strong></p>
+        <USAMap onClick={this.mapHandler} />
+        {/*for loop replacing the "CT" */}
+        <img src={this.state.id ? require('./' + this.state.id + '.png') : ''} alt = '' className={this.state.id !== 'CT' ? 'noShowState' : ''} /> 
+        <USAMap className={this.state.id !== '' ? 'noShowState' : ''} /> 
+      </div>
+    );
+  }
 }
 
 // todo: Fill out red and blue states, either manually or a function
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Title from './Title';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1 className="title">Have I been Gerrymandered?</h1>
-    <Title />
     <App />
   </React.StrictMode>
 );
+
+
+reportWebVitals()
