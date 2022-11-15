b4f442c8fbe62e8d7a3d165ad8b5622f696809ce
Keerti Sundaram <keerti-sundaram@users.noreply.github.com>
2022-11-15 15:58:21 -0500

    algorithm page

---
+++ src/Algorithm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Algorithm extends Component{
+render(){
+    return(
+        <div>
+            <a href = "https://github.com/Have-I-Been-Gerrymandered"> Our Github</a>
+        </div>
+    )
+}
+}
+export default Algorithm;
