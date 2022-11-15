c1d45afcae910eee9f373dd1a557906d82488fd6
swest20 <36340998+swest20@users.noreply.github.com>
2022-11-15 15:28:25 -0500

    All files to include navigation bar

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import USAMap from "react-usa-map";
+import Select from 'react-select';
+import { Route, Routes } from 'react-router-dom';
+import DesignAbout from './DesignAbout';
+import Navbar from './navigationBar';
+import SearchDistrict from './searchDistrict';
+//import Dropdown from 'react-bootstrap/Dropdown';
+import './App.css';
+import './navigationBar.css'
+
+let isStateSelected = false;
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      selectOptions : [],
+      id: '',
+      name: '',
+    }
+  }
+  render() {
+    return (
+      <div class="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<SearchDistrict />} />
+            <Route path="/home" element={<SearchDistrict />} />
+            <Route path="/about" element={<DesignAbout />} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
+}
+
+// todo: Fill out red and blue states, either manually or a function
+// probably not
+export default App;
---
+++ src/navigationBar.js
@@ -0,0 +1,29 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        Have I Been Gerrymandered?
+      </Link>
+      <ul>
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/our-algorithm">Our Algorithm</CustomLink>
+        <CustomLink to="/about">About</CustomLink>
+      </ul>
+    </nav>
+  )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
